149183b72b9fdb0d24863715dfa382c1844b2391
ms-pd01 <162542536+ms-pd01@users.noreply.github.com>
2024-03-07 12:14:37 +0530

    Update streamlit_app.py
    pandas conversion

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,12 @@ st.write('The name of your Smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Covert the snowpark dataframes to Pandas Dataframe
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
